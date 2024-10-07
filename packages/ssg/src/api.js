27e1056646499918a295454688300272cf22b2dd
Rene Kaesler <renekaesler@gmail.com>
2024-10-07 11:33:24 +0200

    feat: upgrade cli with a build option

--- packages/ssg/src/api.js
+++ packages/ssg/src/api.js
@@ -8,3 +8,53 @@ export const serve = async conf => {
 
   await server.listen();
 };
+
+export const build = async conf => {
+  console.log("...build not implemented, yet");
+  // Code idea:
+  // const router = new Router(conf?.router);
+  // const bundler = new StyleBundler(conf);
+  // const pipeline = new AssetPipeline(conf?.dir?.out);
+
+  // const generations = [];
+  // const deferredTasks = [];
+
+  // const generate = async ([route, src], { doc, styles }) => {
+  //   const walker = new DocumentWalker(document);
+  //   const injections = bundler.registerInjector(walker, ...styles);
+  //   const replacements = [];
+  //   walker.on("link[rel=stylesheet][href]", ({ attributes }) => {
+  //     const replacement = pipeline
+  //       .processFile(attributes.href)
+  //       .on("hashed", ({ path }) => (attributes.href = path));
+  //     replacements.push(replacement);
+  //   });
+  //   walker.on("img[src]", async ({ attributes }) => {
+  //     const replacement = pipeline
+  //       .processFile(attributes.src)
+  //       .on("hashed", ({ path }) => (attributes.src = path));
+  //     replacements.push(replacement);
+  //   });
+  //   walker.traverse();
+  //   await Promise.all([...injections, ...replacements]);
+  //   const src = join(conf.dir.routes, `${route}.html`);
+  //   const html = render(document);
+  //   return pipeline.process(html, { src });
+  // };
+
+  // await router.scan(conf?.dir?.pages);
+
+  // for (const [route, path] of router.entries()) {
+  //   const routingResult = await exec(path, {
+  //     url: { pathname: route },
+  //   });
+
+  //   if (routingResult.styles.some(({ bundle }) => isGlobalBundle(bundle))) {
+  //     deferredTasks.push([route, routingResult]);
+  //   } else {
+  //     generations.push(generate([route, path], routingResult));
+  //   }
+  // }
+
+  // return Promise.all(generations);
+};
--- packages/ssg/src/cli.js
+++ packages/ssg/src/cli.js
@@ -1,10 +1,25 @@
 import { join } from "node:path";
+import { program } from "commander";
 
 const configPath = join(process.cwd(), "xylit.config.js");
 
-const [{ default: conf }, { serve }] = await Promise.all([
+const [{ default: conf }, api] = await Promise.all([
   import(configPath).catch(() => ({})),
   import("./api.js"),
 ]);
 
-serve(conf);
+program
+  .name("ssg")
+  .description("CLI to generate static websites with xylit-ssg")
+  .version("0.0.1")
+  .option("--serve", "run the dev server")
+  .option("--build", "generate the website")
+  .action(async ({ serve, build }) => {
+    if (serve) {
+      await api.serve(conf);
+    } else if (build) {
+      await api.build(conf);
+      process.exit();
+    }
+  })
+  .parse();
