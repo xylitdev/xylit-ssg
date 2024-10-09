5b48b41b368a130d19051b866e205ff5de6ebf12
Rene Kaesler <renekaesler@gmail.com>
2024-10-09 09:03:16 +0200

    refactor: outsource ssg compiling from ssg loader

--- packages/ssg/loaders/ssg-loader.js
+++ packages/ssg/loaders/ssg-loader.js
@@ -1,30 +1,8 @@
-import { readFile, stat } from "node:fs/promises";
+import { stat } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import * as path from "node:path";
 
-import { parse } from "acorn";
-import * as walk from "acorn-walk";
-import MagicString from "magic-string";
-
-const injectImports = (source, ast) => {
-  source.prepend(
-    [
-      'import * as SSG from "@xylit/ssg";',
-      "const { html, style } = SSG.init(import.meta);",
-      "export const meta = import.meta;",
-      "",
-    ].join("\n")
-  );
-};
-
-const wrapDefaultExport = (source, ast) => {
-  walk.simple(ast, {
-    ExportDefaultDeclaration({ declaration: { start, end } }) {
-      source.appendLeft(start, "SSG.defineComponent(import.meta, () => (");
-      source.appendRight(end, "));");
-    },
-  });
-};
+import { compile } from "../src/compiler.js";
 
 export async function resolve(specifier, context, nextResolve) {
   if (specifier == "ssg:config") {
@@ -43,19 +21,10 @@ export async function load(url, context, next) {
   if (!url.endsWith(".ssg.js")) return next(url, context);
 
   const path = fileURLToPath(url);
-  const source = new MagicString(await readFile(path, { encoding: "utf-8" }));
-
-  const ast = parse(source.toString(), {
-    sourceType: "module",
-    ecmaVersion: "latest",
-  });
-
-  injectImports(source, ast);
-  wrapDefaultExport(source, ast);
 
   return {
     format: "module",
     shortCircuit: true,
-    source: source.toString(),
+    source: await compile(path),
   };
 }
