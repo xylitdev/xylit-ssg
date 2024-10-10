b33be48308c9117177d38905a9c7f06c28a95875
Rene Kaesler <renekaesler@gmail.com>
2024-10-10 13:27:03 +0200

    refactor:cleanup dev-server code

--- packages/ssg/src/dev-server/server.js
+++ packages/ssg/src/dev-server/server.js
@@ -7,7 +7,6 @@ import { watch } from "chokidar";
 import { parse } from "node-html-parser";
 import mime from "mime";
 
-import { addDependency, dependantsOf } from "../dependencies.js";
 import Router from "../router.js";
 import { exec, kill } from "../runtime.js";
 import { transform } from "../literals/style.js";
@@ -17,6 +16,79 @@ import { fileExists } from "../utils/fs.js";
 import { createLivereload } from "./livereload.js";
 import Server404 from "./server-404.ssg.js";
 import Server500 from "./server-500.ssg.js";
+
+const routeHandler = async (req, res, { livereload, router }) => {
+  const route = router.match(req.url);
+
+  if (!route) return;
+
+  const componentDir = dirname(route.destination);
+
+  const { doc, styles } = await exec(route.destination, { route });
+
+  const node =
+    doc.querySelector("head") ||
+    doc.querySelector("body") ||
+    doc.querySelector("html") ||
+    doc;
+
+  node.insertAdjacentHTML?.(
+    "beforeend",
+    `<style>${styles.map(s => s.source).join("\n")}</style>`
+  );
+
+  doc.querySelectorAll("link[rel=stylesheet]")?.forEach?.(node => {
+    const href = node.getAttribute("href");
+    const filePath = resolve(componentDir, href);
+
+    node.setAttribute("data-dependency", pathToFileURL(filePath).toString());
+  });
+
+  livereload.inject(doc);
+  res.setHeader("Content-Type", "text/html");
+  res.end(doc.toString());
+
+  return true;
+};
+
+const assetHandler = async (req, res) => {
+  if (![".sass", ".scss", ".css"].some(ext => req.url.endsWith(ext))) {
+    return;
+  }
+
+  const filePath = join(process.cwd(), req.url);
+
+  try {
+    const result = await transform(null, { src: filePath });
+
+    res.writeHead(200, mime.getType(req.url));
+    res.end(result.source);
+    return true;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const staticFileHandler = async (req, res, { conf }) => {
+  const staticFilePath = join(conf.static, req.url);
+
+  if (!(await fileExists(staticFilePath))) return;
+
+  const stream = createReadStream(staticFilePath);
+  res.writeHead(200, mime.getType(staticFilePath));
+  stream.pipe(res);
+
+  return true;
+};
+
+const notFoundHandler = async (req, res, { livereload }) => {
+  const doc = await Server404().then(parse);
+  livereload.inject(doc);
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.end(doc.toString());
+
+  return true;
+};
 
 export const createServer = conf => {
   conf = defaults(conf, {
@@ -28,116 +100,28 @@ export const createServer = conf => {
   const server = createHttpServer();
   const livereload = createLivereload({ server });
   const router = new Router();
+  const context = { conf, livereload, router };
+
+  const requestHandlers = [
+    routeHandler,
+    assetHandler,
+    staticFileHandler,
+    notFoundHandler,
+  ];
 
   server.on("request", async (req, res) => {
-    const route = router.match(req.url);
-
-    if (route) {
-      let doc;
-      const componentDir = dirname(route.destination);
-
+    for (const onRequest of requestHandlers) {
       try {
-        let styles;
-
-        ({ doc, styles } = await exec(route.destination, { route }));
-
-        const node =
-          doc.querySelector("head") ||
-          doc.querySelector("body") ||
-          doc.querySelector("html") ||
-          doc;
-
-        node.insertAdjacentHTML?.(
-          "beforeend",
-          `<script>
-            window.addEventListener('xylit:livereload', ({ detail: { type, payload }}) => {
-              switch (type) {
-                case 'change':
-                  payload?.forEach(dependency => {
-                    const sheet = document.querySelector(\`[data-dependency="\${dependency}"\`)
-                   
-                    if(!sheet) return;
-
-                    const newSheet = sheet.cloneNode();
-                    const removeOldSheet = e => { sheet.remove(); }
-
-                    newSheet.addEventListener('load', removeOldSheet);
-                    newSheet.addEventListener('error', removeOldSheet);
-                    newSheet.setAttribute('href', sheet.getAttribute('href'))
-                    sheet.insertAdjacentElement("afterend", newSheet);
-                  });
-              }
-            });
-          </script>`
-        );
-
-        node.insertAdjacentHTML?.(
-          "beforeend",
-          `<style>${styles.map(s => s.source).join("\n")}</style>`
-        );
-
-        doc.querySelectorAll("link[rel=stylesheet]")?.forEach?.(node => {
-          const href = node.getAttribute("href");
-          const filePath = resolve(componentDir, href);
-
-          node.setAttribute(
-            "data-dependency",
-            pathToFileURL(filePath).toString()
-          );
-        });
-
-        res.setHeader("Content-Type", "text/html");
+        if (await onRequest(req, res, context)) return;
       } catch (e) {
         console.error(e);
-        doc = await Server500().then(parse);
-        res.writeHead(500, { "Content-Type": "text/html" });
-      }
 
-      livereload.inject(doc);
-      return res.end(doc.toString());
-    }
-
-    if ([".sass", ".scss", ".css"].some(ext => req.url.endsWith(ext))) {
-      const filePath = join(process.cwd(), req.url);
-
-      try {
-        const result = await transform(null, { src: filePath });
-
-        addDependency(
-          pathToFileURL(filePath).toString(),
-          ...result.dependencies.map(url => url.toString())
-        );
-
-        res.writeHead(200, mime.getType(req.url));
-        res.end(result.source);
-      } catch (e) {
-        console.error(e);
-        const doc = await Server404().then(parse);
+        const doc = await Server500().then(parse);
         livereload.inject(doc);
-
-        res.writeHead(404, { "Content-Type": "text/html" });
+        res.writeHead(500, { "Content-Type": "text/html" });
         res.end(doc.toString());
       }
-
-      return;
     }
-
-    const staticFilePath = join(conf.static, req.url);
-
-    if (await fileExists(staticFilePath)) {
-      const stream = createReadStream(staticFilePath);
-
-      res.writeHead(200, mime.getType(staticFilePath));
-      stream.pipe(res);
-
-      return;
-    }
-
-    const doc = await Server404().then(parse);
-    livereload.inject(doc);
-
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end(doc.toString());
   });
 
   watch(process.cwd(), {
@@ -145,20 +129,10 @@ export const createServer = conf => {
     recursive: true,
     ignoreInitial: true,
     ignored: file => file.includes("node_modules"),
-  }).on("all", async (event, file) => {
+  }).on("all", async () => {
     kill();
     await router.scan(conf.in);
-
-    const fileUrl = pathToFileURL(file).toString();
-
-    if (fileUrl.endsWith(".scss")) {
-      // first test of css hot reloading (for now only scss files)
-      const dependants = dependantsOf(fileUrl);
-
-      livereload.send("change", [fileUrl, ...dependants]);
-    } else {
-      livereload.send("reload");
-    }
+    livereload.send("reload");
   });
 
   return {
