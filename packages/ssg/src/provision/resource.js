1aa5b6a996f2b0d6f16bc3bdf3a88232de40a672
Rene Kaesler <renekaesler@gmail.com>
2024-11-03 22:57:24 +0800

    refactor: remove double class

--- packages/ssg/src/provision/resource.js
+++ packages/ssg/src/provision/resource.js
@@ -27,29 +27,3 @@ export class Resource {
     });
   }
 }
-
-export class ResourceProcessor {
-  transforms = [];
-
-  addTransform(condition, transform) {
-    const entry = [condition, transform];
-
-    if (typeof condition === "string") {
-      entry[0] = ({ mediaType }) => mediaType;
-    } else if (Array.isArray(condition)) {
-      entry[0] = ({ mediaType }) => condition.includes(mediaType);
-    }
-
-    this.transforms.unshift(entry);
-  }
-
-  async transform(resource) {
-    for (const [condition, transform] of this.transforms) {
-      if (!condition(resource)) continue;
-
-      return transform(resource);
-    }
-
-    return resource;
-  }
-}
